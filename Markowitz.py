8d3e490f71745894d45825d9059f3d547aa9a1e5
LouisTsai <q1030176@gmail.com>
2024-05-03 10:27:47 +0800

    feat(problem 1): add project setup section

--- Markowitz.py
+++ Markowitz.py
@@ -9,3 +9,41 @@ import quantstats as qs
 import gurobipy as gp
 import argparse
 import warnings
+
+"""
+Project Setup
+"""
+warnings.simplefilter(action="ignore", category=FutureWarning)
+
+assets = [
+    "SPY",
+    "XLB",
+    "XLC",
+    "XLE",
+    "XLF",
+    "XLI",
+    "XLK",
+    "XLP",
+    "XLRE",
+    "XLU",
+    "XLV",
+    "XLY",
+]
+
+start = "2019-01-01"
+end = "2024-04-01"
+data = pd.DataFrame()
+
+# Fetch the data for each stock and concatenate it to the `data` DataFrame
+for asset in assets:
+    raw = yf.download(asset, start=start, end=end)
+    raw["Symbol"] = asset
+    data = pd.concat([data, raw], axis=0)
+
+# Initialize df and df_returns
+df = portfolio_data = data.pivot_table(
+    index="Date", columns="Symbol", values="Adj Close"
+)
+df_returns = df.pct_change().fillna(0)
+
+
